d2364f2a7844bf7db2f09a4d3da61d08e67c190a
Florin Lazar <florin.lazar.it2014@gmail.com>
2024-04-22 16:20:33 +0300

    discuss about floating point numbers

--- ValueTypes/Program.cs
+++ ValueTypes/Program.cs
@@ -10,6 +10,69 @@ namespace ValueTypes
     {
         static void Main(string[] args)
         {
+            // illustrate division by zero (with sign)
+            // for float and double
+            float a = 10;
+            float b = -0F;
+            float c = a / b;
+            Console.WriteLine(c);
+
+            // but decimal throws error
+            decimal x = 10;
+            decimal y = 0;
+            decimal z = x / y;
+            Console.WriteLine(z);
+
+            Console.WriteLine("Press any key to close");
+            Console.ReadKey();
+        }
+
+        static void Examples_with_int()
+        {
+            // illustrate pre-fix / post-fix increment operators
+            int x = 10;
+            Console.WriteLine("Value before: " + x);
+            int result = ++x;
+            Console.WriteLine("Result: " + result);
+            Console.WriteLine("Value after: " + x);
+
+            // illustrate division for integers - vs - real numbers
+            unchecked
+            {
+                float a = 5;
+                int a2 = (int)float.MaxValue;
+                Console.WriteLine("A2=" + a2);
+                int b = 2;
+                float c = (float)a / b;
+                Console.WriteLine(c);
+            }
+
+            // arithmetic overflow
+            Add(uint.MaxValue, 1);
+            checked
+            {
+                int z = int.MaxValue;
+                Console.WriteLine("Z = " + z);
+                z = z + 1;
+                Console.WriteLine("Z = " + z);
+            }
+        }
+
+        static void Add(uint x, uint y)
+        {
+            if (x < uint.MaxValue - y)
+            {
+                uint result = x + y;
+                Console.WriteLine("Result: " + result);
+            }
+            else
+            {
+                Console.WriteLine("Result: Arithmetic overflow");
+            }
+        }
+
+        static void Examples_with_bool()
+        {
             bool bitwiseAnd = ReturnValue(false) & ReturnValue(true);
             Console.WriteLine(" ----------- ");
             bool conditionalAnd = ReturnValue(false) && ReturnValue(true);
