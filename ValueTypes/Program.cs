7602f3c759e454c3f9ed683167b3875647c86935
Florin Lazar <florin.lazar.it2014@gmail.com>
2024-04-22 15:10:33 +0300

    exemplify the difference between bitwise and conditional operators

--- ValueTypes/Program.cs
+++ ValueTypes/Program.cs
@@ -10,6 +10,35 @@ namespace ValueTypes
     {
         static void Main(string[] args)
         {
+            bool bitwiseAnd = ReturnValue(false) & ReturnValue(true);
+            Console.WriteLine(" ----------- ");
+            bool conditionalAnd = ReturnValue(false) && ReturnValue(true);
+
+            // conditional and enables the following verification
+            string text = null;
+            if (text != null && text.Length > 2)
+            {
+                Console.WriteLine("Text has more than 2 characters");
+            }
+
+
+            Console.WriteLine(" ----------- ");
+            Console.WriteLine(" ----------- ");
+
+            bool bitwiseOr = ReturnValue(true) | ReturnValue(false);
+
+            Console.WriteLine(" ----------- ");
+
+            bool conditionalOr = ReturnValue(true) || ReturnValue(false);
+
+            Console.WriteLine("Press any key to close");
+            Console.ReadKey();
+        }
+
+        static bool ReturnValue(bool value)
+        {
+            Console.WriteLine("Called ReturnValue with: " + value);
+            return value;
         }
     }
 }
