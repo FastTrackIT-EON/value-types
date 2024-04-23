2bd488a94437f0a3596600f97993791a19330726
Florin Lazar <florin.lazar.it2014@gmail.com>
2024-04-23 11:28:49 +0300

    examples with enums

---
+++ ValueTypes/Enums.cs
@@ -0,0 +1,36 @@
+﻿namespace ValueTypes
+{
+    public enum DayOfWeek
+    {
+        None = 0,
+        Luni = 1 << 0,
+        Marti = 1 << 1,
+        Miercuri = 1 << 2,
+        Joi = 1 << 3,
+        Vineri = 1 << 4,
+        Sambata = 1 << 5,
+        Duminica = 1 << 6,
+
+        WorkingDays = DayOfWeek.Luni | DayOfWeek.Marti | DayOfWeek.Miercuri 
+                      | DayOfWeek.Joi,
+        Weekend = DayOfWeek.Vineri | DayOfWeek.Sambata | DayOfWeek.Duminica
+    }
+
+    public enum AccessLevel
+    {
+        None = 0,
+        Read = 1 << 0,
+        Write = 1 << 2,
+
+        ReadAndWrite = AccessLevel.Read | AccessLevel.Write
+    }
+
+    public enum IdDocumentTypes
+    {
+        IdCard = 0,
+        Passport,
+        NewDocument,
+        DrivingLicense,
+
+    }
+}
--- ValueTypes/Program.cs
+++ ValueTypes/Program.cs
@@ -10,7 +10,48 @@ namespace ValueTypes
 {
     internal class Program
     {
+        const string Saturday = "Saturday";
+
         static void Main(string[] args)
+        {
+
+            int dayOfWeek = (int)DayOfWeek.Miercuri;
+           
+
+
+            DayOfWeek otherDayOfWeek = (DayOfWeek)100;
+            if (Enum.IsDefined(typeof(DayOfWeek), 100))
+            {
+                DayOfWeek correctDayOfWeek = (DayOfWeek)100;
+                Console.Write(correctDayOfWeek);
+            }
+            else
+            {
+                Console.WriteLine("Value 100 is not defined");
+            }
+
+            DayOfWeek parsedDayOfWeek;
+            if(Enum.TryParse("Marti", true, out parsedDayOfWeek))
+            {
+                Console.WriteLine(parsedDayOfWeek);
+            }
+            else
+            {
+                Console.WriteLine("Value 'Marti' is not defined");
+            }
+
+            DayOfWeek dayToTest = DayOfWeek.Joi;
+            if ((DayOfWeek.Weekend & dayToTest) == dayToTest)
+            {
+                Console.WriteLine(dayToTest + " is part of the weekend");
+            }
+
+
+            Console.WriteLine("Press any key to close");
+            Console.ReadKey();
+        }
+
+        static void Examples_with_DateTime_and_TimeStamp()
         {
             DateTime dateTime = DateTime.Now;
 
@@ -23,16 +64,19 @@ namespace ValueTypes
             Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture("fa");
             Console.WriteLine("Date (fa): " + dateTime.ToString());
 
-           
-
             DateTime d1 = new DateTime(year: 2024, month: 4, day: 22);
             DateTime d2 = d1 + TimeSpan.FromDays(2);
             TimeSpan d3 = d2 - d1;
 
+            Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture("en-US");
             DateTime parseResult1;
-            bool canParse = DateTime.TryParse(
-                "4/22/2024",
-                CultureInfo.CreateSpecificCulture("en-US"),
+            bool canParse = DateTime.TryParseExact(
+                "4-22-2024",
+                new string[] {
+                    "M/dd/yyyy", "MM/dd/yyyy",
+                    "dd-M-yyyy", "dd-MM-yyyy",
+                    "MM-dd-yyyy", "M-dd-yyyy" },
+                CultureInfo.InvariantCulture.DateTimeFormat,
                 DateTimeStyles.None,
                 out parseResult1);
 
@@ -47,10 +91,11 @@ namespace ValueTypes
                 Console.WriteLine("String is not a valid DateTime");
             }
 
-            
-
-            Console.WriteLine("Press any key to close");
-            Console.ReadKey();
+            TimeSpan ts = TimeSpan.FromDays(2) + TimeSpan.FromHours(25);
+            Console.WriteLine("Timespan Days:" + ts.Days);
+            Console.WriteLine("Timespan Total Days:" + ts.TotalDays);
+            Console.WriteLine("Timespan Hours:" + ts.Hours);
+            Console.WriteLine("Timespan Total Hours:" + ts.TotalHours);
         }
 
         static void Examples_with_floating_point_numbers()
